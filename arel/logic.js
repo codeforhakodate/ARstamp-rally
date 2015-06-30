514c9798f69c3b89c198b2111c0e99a2da42dcd7
kudotakuya <ttt.ttt.7733@gmail.com>
2015-07-01 00:41:14 +0900

    ボタンを追加

---
+++ arel/logic.js
@@ -0,0 +1,16 @@
+arel.sceneReady(function()
+{
+	//Just for Debuging purposes
+	//arel.Debug.activate();
+	//arel.Debug.deactivateArelLogStream();
+	
+	//Create the ammunition display
+	
+});
+
+
+//This function will be triggered if the user is pressing the Shoot Button
+function shoot()
+{
+
+};
