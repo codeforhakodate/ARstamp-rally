43669a9f41e133f956577f3dbb337035ed95ddc1
kudotakuya <ttt.ttt.7733@gmail.com>
2015-07-02 09:29:07 +0900

    スタンプ確認ボタンを追加

--- arel/logic.js
+++ arel/logic.js
@@ -8,9 +8,28 @@ arel.sceneReady(function()
 	
 });
 
-
+var i = 0;
 //This function will be triggered if the user is pressing the Shoot Button
 function shoot()
 {
 
+//alert("stamp");
+//document.write('<img src="bullet.png" width="104" height="291" />');
+if(i%2==0){
+document.kuji.src = "bullet.png"; 
+}else{
+document.kuji.src = "aaa.png"; 
+alert("test");
+}
+i++;
+//document.write('<a href="#" class="Button" onclick="close()">隠す</a>');
+
+//arel.Events.setListener(metaioman, function(obj, type, params){handleMetaioManEvents(obj, type, params);});
+//obj.startAnimation("close_down");
+
 };
+
+function close(){
+
+	location.href = "/index.html";
+}
